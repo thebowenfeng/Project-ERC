63fc2e69ea38e2cbf80343ce4dbf26d8874fa572
Fengax <857514.leofeng@gmail.com>
2022-07-27 14:31:28 +1000

    improved time find algorithm speed, fixed time check bugs

--- main.py
+++ main.py
@@ -175,109 +175,102 @@ if __name__ == "__main__":
         WebDriverWait(driver, 10).until(
             expected_conditions.presence_of_element_located((By.ID, "startTime")))
 
-        start_time_num = len(driver.find_element(By.ID, "startTime").find_elements(By.TAG_NAME, "option"))
-        i = 0
-
         room_name = driver.find_element(By.ID, "dialog0").find_element(By.CLASS_NAME, "emphatic").text
 
         time.sleep(0.1)
 
         print(f"Looking at room {room_name} on date {get_date()}")
 
-        while i < start_time_num:
+        free_times = populate_free(get_date())
+
+        for time_slot in free_times:
             WebDriverWait(driver, 10).until(
                 expected_conditions.presence_of_element_located((By.ID, "startTime")))
 
             while True:
                 try:
-                    option = driver.find_element(By.ID, "startTime").find_elements(By.TAG_NAME, "option")[i]
-                    break
-                except:
-                    pass
-                
-            time_select = Select(driver.find_element(By.ID, "startTime"))
-
-            while True:
-                try:
-                    start_time = option.get_attribute("value")
+                    time_options = list(map(lambda x: x.get_attribute("value"),
+                                       driver.find_element(By.ID, "startTime").find_elements(By.TAG_NAME, "option")))
                     break
                 except:
                     pass
 
-            if START <= datetime.strptime(start_time, "%H:%M:%S") <= END:
-                time_select.select_by_value(start_time)
+            prev_end_time = None
+            for start_time in time_options:
+                start_time_dt = datetime.strptime(start_time, "%H:%M:%S")
 
-                time.sleep(0.3)
+                if start_time_dt >= time_slot[1]:
+                    break
 
-                while True:
-                    try:
-                        end_time_select = Select(driver.find_element(By.ID, "endTime"))
-                        end_time = driver.find_element(By.ID, "endTime").find_elements(By.TAG_NAME, "option")[
-                            -1].get_attribute(
-                            "value")
-                        break
-                    except:
-                        pass
+                if prev_end_time is not None and start_time_dt < datetime.strptime(prev_end_time, "%H:%M:%S"):
+                    continue
 
-                if check_time((start_time, end_time), get_date()):
+                if start_time_dt >= time_slot[0] and START <= start_time_dt <= END:
+                    Select(driver.find_element(By.ID, "startTime")).select_by_value(start_time)
+                    time.sleep(0.3)
+
                     while True:
                         try:
-                            end_time_select.select_by_value(end_time)
-                            driver.find_element(By.ID, "submitButton").click()
+                            end_time_options = list(map(lambda x: x.get_attribute("value"),
+                                       driver.find_element(By.ID, "endTime").find_elements(By.TAG_NAME, "option")))
                             break
                         except:
                             pass
 
-                    time.sleep(1)
-
-                    try:
-                        text = driver.find_element(By.CLASS_NAME, "expectedException").text
-                        if text != "":
-                            if text == "Bookings are limited to one hour per day per site.":
-                                print("Site booking exceeded")
-                                driver.find_element(By.CLASS_NAME, "dialogClose").click()
-
-                                return "break"
-                            else:
-                                print(text)
-                                '''
-                                time.sleep(0.5)
-                                driver.quit()
-                                time.sleep(0.5)
-                                sys.exit(0)
-                                '''
-                                while True:
+                    for end_time in reversed(end_time_options):
+                        prev_end_time = end_time
+                        if check_time((start_time, end_time), get_date()):
+                            while True:
+                                try:
+                                    Select(driver.find_element(By.ID, "endTime")).select_by_value(end_time)
+                                    driver.find_element(By.ID, "submitButton").click()
+                                    break
+                                except:
                                     pass
-                    except NoSuchElementException as e:
-                        print(f"Found a slot for {room_name} at {start_time} - {end_time}")
-                        insert_reserved_time((start_time, end_time), get_date())
 
-                        room_code = None
-                        for code in ROOM_NAMES:
-                            if code in room_name:
-                                room_code = code
+                            time.sleep(1)
 
-                        res = requests.post(url="http://mangotests.asuscomm.com/recordnewbooking",
-                                      data={"CurrentDate": datetime.strptime(get_date(), "%d/%m/%Y").strftime("%Y-%m-%d"),
-                                            "StartTime": start_time,
-                                            "EndTime": end_time,
-                                            "RoomNumber": room_code,
-                                            "BookedBy": user},
-                                      headers={"Content-Type": "application/x-www-form-urlencoded"})
+                            try:
+                                text = driver.find_element(By.CLASS_NAME, "expectedException").text
+                                if text != "":
+                                    if text == "Bookings are limited to one hour per day per site.":
+                                        print("Site booking exceeded")
+                                        driver.find_element(By.CLASS_NAME, "dialogClose").click()
 
-                        print(res.text)
+                                        return "break"
+                                    else:
+                                        print(text)
+                                        '''
+                                        time.sleep(0.5)
+                                        driver.quit()
+                                        time.sleep(0.5)
+                                        sys.exit(0)
+                                        '''
+                                        while True:
+                                            pass
+                            except NoSuchElementException as e:
+                                print(f"Found a slot for {room_name} at {start_time} - {end_time}")
+                                insert_reserved_time((start_time, end_time), get_date())
 
-                    return True
+                                room_code = None
+                                for code in ROOM_NAMES:
+                                    if code in room_name:
+                                        room_code = code
 
-            while True:
-                try:
-                    start_time_num = len(driver.find_element(By.ID, "startTime").find_elements(By.TAG_NAME, "option"))
-                    break
-                except:
-                    pass
-            i += 1
+                                res = requests.post(url="http://mangotests.asuscomm.com/recordnewbooking",
+                                                    data={"CurrentDate": datetime.strptime(get_date(),
+                                                                                           "%d/%m/%Y").strftime(
+                                                        "%Y-%m-%d"),
+                                                        "StartTime": start_time,
+                                                        "EndTime": end_time,
+                                                        "RoomNumber": room_code,
+                                                        "BookedBy": user},
+                                                    headers={"Content-Type": "application/x-www-form-urlencoded"})
 
-        print("done")
+                                print(res.text)
+
+                            return True
+
         driver.find_element(By.CLASS_NAME, "dialogClose").click()
         time.sleep(0.1)
 
